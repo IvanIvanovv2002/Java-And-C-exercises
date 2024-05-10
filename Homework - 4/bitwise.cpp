c50abf3e8f54141e98e83db6a20790736cfe9f18
IvanIvanovv2002 <145019587+IvanIvanovv2002@users.noreply.github.com>
2024-05-10 20:51:31 +0300

    Add files via upload

---
+++ Homework - 4/bitwise.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <cstdlib>
+#include <bitset>
+#include <cstdio>
+
+bool validNumber(int num) {
+    return 0 <= num && 63 >= num;
+}
+
+void setBit(uint64_t& attendance,int student) {
+    if (!validNumber(student)) {
+        std::cerr << "Student number must be between 0 and 63 !";
+        return;
+    }
+     attendance |= (1ULL << student);
+}
+
+void clearBit(uint64_t& attendance,int student) {
+    if (!validNumber(student)) {
+        std::cerr << "Student number must be between 0 and 63 !";
+        return;
+    }
+    attendance &= ~(1ULL << student);
+}
+
+void flipBit(uint64_t& attendance,int student) {
+    if (!validNumber(student)) {
+        std::cerr << "Student number must be between 0 and 63 !";
+        return;
+    }
+    attendance ^= (1ULL << student);
+}
+
+bool readBit(uint64_t& attendance,int student) {
+    if (!validNumber(student)) {
+        std::cerr << "Student number must be between 0 and 63 !";
+    }
+    return attendance & (1ULL << student);
+}
+
+void searchAttendedStudents(uint64_t& attendance) {
+    for (int i = 0; i < 64; i ++) {
+        if (readBit(attendance,i)) {
+            std::cout << "Student " << i + 1 << "is here !" << std::endl;
+        }
+    }
+}
+
+void searchMissingStudents(uint64_t& attendance) {
+    for (int i = 0; i < 64; i ++) {
+        if (!readBit(attendance,i)) {
+            std::cout << "Student " << i + 1 << " isn't there !" << std::endl;
+        }
+    }
+}
+
+
+int main(void) {
+    uint64_t attendance = 0;
+    int option;
+
+    std::cout << "1: Set attendance" << std::endl;
+    std::cout << "2: Clear attendance" << std::endl;
+    std::cout << "3: Attending students" << std::endl;
+    std::cout << "4: Absent students" << std::endl;
+    std::cout << "5: Change attendance status" << std::endl;
+    std::cout << "6: Exit" << std::endl;
+
+    while (1) {
+        int studentNumber;
+        std::cin >> option;
+        
+        switch (option) {
+        case 1:
+            std::cin >> studentNumber;
+            setBit(attendance,studentNumber);
+            std::cout << std::bitset<8 * sizeof(attendance)>(attendance) << std::endl ;
+            break;
+        case 2:
+            std::cin >> studentNumber;
+            clearBit(attendance,studentNumber);
+            std::cout << std::bitset<8 * sizeof(attendance)>(attendance) << std::endl; 
+            break;
+        case 3:
+            searchAttendedStudents(attendance);
+            break;
+        case 4:
+            searchMissingStudents(attendance);
+            break;
+        case 5:
+             std::cin >> studentNumber;
+            flipBit(attendance,studentNumber);
+            std::cout << std::bitset<8 * sizeof(attendance)>(attendance) << std::endl; 
+            break;
+        case 6:
+            std::cout << "End of program." << std::endl;
+            return 0;
+        default:
+            std::cerr << "The options for numbers are 1-6, do it again." << std::endl;
+            continue;
+        }
+    }
+    return 0;
+}
